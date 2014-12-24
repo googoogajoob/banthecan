06568399fe5bf2d490466ebbc9308ac52b599f82
Andrew Potter <apc@andypotter.org>
2014-12-24 23:44:24 +0100

    Added Tickets inside of columns

--- frontend/views/site/kanbanboard.php
+++ frontend/views/site/kanbanboard.php
@@ -8,14 +8,34 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-kanbanboard">
     <h1><?= Html::encode($this->title) ?></h1>
 <?php
-$css = 'width: 200px;
-        height: 600px;
-        border-style: solid;
-        border-color: black;
-        margin: 10px;
-        padding: 1em;
-        float: left;';
-$kanbancolumn = '<div style="' . $css . '">This is a column</div>';
+$columnStyle = '
+    width: 200px;
+    height: 600px;
+    background-color: lightgray;
+    border-style: solid;
+    border-color: black;
+    border-width: 1px;
+    margin: 10px;
+    padding: 5px;
+    float: left;';
+
+$ticketStyle = '
+    width: 188px;
+    height: 100px;
+    background-color: #fff;
+    border-style: solid;
+    border-color: blue;
+    border-width: 1px;
+    padding: 5px;
+    margin-bottom: 4px;
+    float: left;';
+
+
+$kanbancolumn = '<div style="' . $columnStyle . '">This is a column';
+for($i=0; $i<4; $i++) {
+    $kanbancolumn .= '<div style="' . $ticketStyle . '">This is a Ticket' . '</div>';
+}
+$kanbancolumn .= '</div>';
 for($i=0; $i<5; $i++) {
     echo $kanbancolumn;
 }
