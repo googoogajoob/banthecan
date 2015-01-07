0b496156a8089b230a3f5d29400f510ac8373438
Andrew Potter <apc@andypotter.org>
2015-01-07 23:31:08 +0100

    Sitenames for FE and BE changed

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'Ban the Can',
+                'brandLabel' => 'Ban the Can - FRONTEND',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
