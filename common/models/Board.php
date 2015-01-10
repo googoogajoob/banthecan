23eed2b8b9c916fef5003f822ee370542fd3fae9
Andrew Potter <apc@andypotter.org>
2015-01-10 21:35:00 +0100

    gii generated code

---
+++ common/models/Board.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "board".
+ *
+ * @property integer $id
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $title
+ * @property string $description
+ * @property integer $max_lanes
+ *
+ * @property BoardColumn[] $boardColumns
+ */
+class Board extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'board';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'created_at', 'updated_at', 'title', 'description', 'max_lanes'], 'required'],
+            [['id', 'created_at', 'updated_at', 'max_lanes'], 'integer'],
+            [['title', 'description'], 'string']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'title' => 'Title',
+            'description' => 'Description',
+            'max_lanes' => 'Max Lanes',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBoardColumns()
+    {
+        return $this->hasMany(BoardColumn::className(), ['board_id' => 'id']);
+    }
+}
