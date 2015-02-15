f7d984ae53c49a4d560f5c2e27ccbd2bc79200c0
Andrew Potter <apc@andypotter.org>
2015-02-15 16:40:46 +0100

    123

--- frontend/views/ticket/_ticketBlockWidget.php
+++ frontend/views/ticket/_ticketBlockWidget.php
@@ -10,22 +10,13 @@ use yii\helpers\Url;
 ?>
 
 <?php
-Draggable::begin([
-    'options' => [
-        'class' => 'ticket-div',
-    ],
-    'clientOptions' => [
-        //'grid' => [10, 10],
-        'cursor' => 'move',
-//        'revert' => true,
-        'zIndex' => 100,
-    ],
-]);
 $ticketViewUrl = Url::to(['ticket/view', 'id' => $ticketRecord['ticketId']]);
 ?>
 
+ob_start();
+
 <div class="draggable-ticket-avatar-div">
-<img src="/images/content/30x40/user-<?php echo $ticketRecord['assignedId']?>.jpg"/>
+    <img src="/images/content/30x40/user-<?php echo $ticketRecord['assignedId']?>.jpg"/>
 </div>
 <strong><a href="<?php echo $ticketViewUrl; ?>"><?php echo $ticketRecord['title']?></a></strong><br />
 
@@ -35,6 +26,3 @@ $ticketViewUrl = Url::to(['ticket/view', 'id' => $ticketRecord['ticketId']]);
 <br /><br />
 <?php echo StringHelper::truncate($ticketRecord['description'], 100, ' ...'); ?>
 
-<?php
-Draggable::end();
-?>
