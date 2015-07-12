adc628e8bccb58b7b0d36d5c538834dc74fb2f77
Andrew Potter <apc@andypotter.org>
2015-07-13 01:14:38 +0200

    Upgrade local to Suse 13.2, New Servers, Workflow

--- frontend/views/ticket/ticketTooltip.js
+++ frontend/views/ticket/ticketTooltip.js
@@ -2,10 +2,13 @@
  * Created by and on 4-May-15.
  */
 
-
-/* For Some reason this is nit needed
-* Perhaps it's already being done from another module??? */
-
-/*$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-}); */
+$(function () {
+    $("[data-toggle-click='tooltip']").tooltip({
+        placement: 'auto',
+        trigger: 'click'
+    });
+    $("[data-toggle-hover='tooltip']").tooltip({
+        placement: 'auto',
+        trigger: 'hover'
+    });
+});
