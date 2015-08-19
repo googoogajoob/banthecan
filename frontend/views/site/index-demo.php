2bc3dd49f31a9bf868fe05d3860eaa5bda97770d
Andrew Potter <apc@andypotter.org>
2015-08-20 01:23:03 +0200

    Demo Frontpage

---
+++ frontend/views/site/index-demo.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<div class="site-index">
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-1">
+                <img src="/images/mockup-home.png"/>
+            </div>
+        </div>
+
+    </div>
+</div>
