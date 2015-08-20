85fc15b5b56a76353ebc6f6b5185af04a1a16d91
Andrew Potter <apc@andypotter.org>
2015-08-20 22:55:10 +0200

    Frontend Demo Screen, Backend CSS up to FE Standard

--- frontend/views/site/index-demo.php
+++ frontend/views/site/index-demo.php
@@ -2,20 +2,25 @@
 /* @var $this yii\web\View */
 ?>
 <div class="site-index">
+
     <div class="jumbotron">
         <h1>Welcome to Ban-The-Can</h1>
+
         <p>
             <strong>Ban-The-Can</strong> is a deliberate play on words of the <a href="https://en.wikipedia.org/wiki/Kanban">Kanban scheduling system.</a>
             Please explore it's features and discover if it is useful for your needs.
             It is an opensource project and therefore free to use. For more detailed information please refer to the
             <a href="/site/about">About page.</a>
         </p>
-        <p>
-            If you discover that <strong>Ban-The-Can</strong> is something for you there are two options available.
-            <ul>
-                <li>Download the software from <a href="https://github.com/gooGooGaJoob/banthecan">GitHub</a> to create for use on your own server</li>
-                <li>Join the public <a href="http://www.ban-the-can.net/">Ban-The-Can Website</a> and manage your organizational needs from there.</li>
-            </ul>
-        </p>
+
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 lead">
+                <h2>If <strong>Ban-The-Can</strong> is something for you two options available.</h2>
+                    <ul class="text-left col-md-offset-1">
+                        <li>Download the software from <a href="https://github.com/gooGooGaJoob/banthecan">GitHub</a> for use on your own server</li>
+                        <li>Join the <a href="http://www.ban-the-can.net/">Ban-The-Can Website</a> to manage your organizational needs</li>
+                    </ul>
+            </div>
+        </div>
     </div>
 </div>
