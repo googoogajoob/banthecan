15749c3bb747e9928913b4ddf60945005a912855
Andrew Potter <apc@andypotter.org>
2015-08-30 19:00:50 +0200

    Work from the Garden

--- common/models/ticketDecoration/TicketDecorationInterface.php
+++ common/models/ticketDecoration/TicketDecorationInterface.php
@@ -8,6 +8,34 @@ namespace common\models\ticketDecoration;
  * The decorations are implemented as Yii-behaviors to the column class,
  * thus the necessary functionality for Yii-behaviors is also required
  *
+ * The Ticket Decoration Concept
+ * =============================
+ * A ticket decoration is an optional functionality of a ticket for performing a specialized task. Examples would be
+ * Blocking/Allowing the movement of a ticket to another column based on specific conditions, create another object such as a task
+ * from a ticket or voting on a protocol before a ticket can be completed.
+ *
+ * Decorations are to be implemented as behaviors of the ticket class. Therefore the interact with the ticket, its view and
+ * its 'business' logic  However, the existence of a decoration in a ticket is determined by the column where a ticket is 
+ * located. Columns contain a set of decoration requirements (and/or) possibilities. As a ticket is moved from one column to 
+ * another, the column dictates which behaviors a ticket can (or must) implement.
+ *
+ * The, the requirements of the various participants is as follows:
+ * ----------------------------------------------------------------
+ * Columns:
+ *     Maintin a list of decorations that are to be implemented in this column (CRUD)
+ *     Persistance (likely with a serialized of CSV field of Decoration Type-IDs)
+ *     Tell Tickets what behaviors are required of them (clear behaviors of a ticket, when it is moved to a new column) 
+ *     Currently The columns determine where tickets can move  
+ *
+ * Tickets:
+ *     Must be able to dynamicaly allow the implementation of any behavior
+ *     Must allow for persistance of a behaviors status for that ticket (serialized field)
+ * 
+ * Behaviors:
+ *     Uniquely Identify itself
+ *		 Provide its name and description 
+ *     Perform all MVC functions that a behavior needs
+ *
  * @author Andrew Potter <apc@andypotter.org>
  */
 interface TicketDecorationInterface {
@@ -17,6 +45,10 @@ interface TicketDecorationInterface {
 
 /* Controller Related Methods */
 /* ========================== */
+
+	/** Currently Empty, no Controller functions 
+	 *  As the decorations are behaviors I see no need for controller functioanlity (yet?)
+	 */ 
 
 /* MODEL Related Methods */
 /* ===================== */
