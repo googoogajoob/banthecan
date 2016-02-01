39648926c9d42146a9ca454f4bf7fe5b784f6dfa
Andrew Potter <apc@andypotter.org>
2016-02-01 23:01:18 +0100

    Another Phase: Test A or B

--- common/models/ticketDecoration/TicketDecorationManager.php
+++ common/models/ticketDecoration/TicketDecorationManager.php
@@ -6,7 +6,7 @@
  * Time: 8:22 PM
  */
 
-namespace common\models\ticketDecoration;
+namespace array_fliparray_flipcommon\models\ticketDecoration;
 
 use common\models\Ticket;
 use yii;
@@ -15,73 +15,73 @@ use yii\base\Object;
 
 class TicketDecorationManager extends Object {
 
-    private $_activeTicketDecorations = [];
-    private $_availableTicketDecorations = [];
+	private $_activeTicketDecorations = [];
+	private $_availableTicketDecorations = [];
 
 
-    /**
-     * @param $decorations array
-     * @return $this common\models\ticketDecoration\TicketDecorationManager
-     */
-    public function setAvailableTicketDecorations($decorations) {
-        $this->_availableTicketDecorations = $decorations;
+	/**
+	 * @param $decorations array
+	 * @return $this common\models\ticketDecoration\TicketDecorationManager
+	 */
+	public function setAvailableTicketDecorations($decorations) {
+		$this->_availableTicketDecorations = $decorations;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return array Keys, i.e. class names of available decorations
-     */
-    public function getAvailableTicketDecorations() {
-        $junk = array_keys($this->_availableTicketDecorations);
-        return $junk;
-    }
+	/**
+	 * @return array Keys, i.e. class names of available decorations
+	 */
+	public function getAvailableTicketDecorations() {
+		$junk = array_keys($this->_availableTicketDecorations);
+		return $junk;
+	}
 
-    /**
-     * Returns the configuration arrays for the current set of active ticket decorations
-     *
-     * @return array
-     */
-    public function getActiveTicketDecorations() {
-        return $this->_activeTicketDecorations;
-    }
+	/**
+	 * Returns the configuration arrays for the current set of active ticket decorations
+	 *
+	 * @return array
+	 */
+	public function getActiveTicketDecorations() {
+		return $this->_activeTicketDecorations;
+	}
 
-    /**
-     * Is $className an available ticket decoration
-     *
-     * @return boolean
-     */
-    public function isAvailable($className) {
-        return array_key_exists($className, $this->_availableTicketDecorations);
-    }
+	/**
+	 * Is $className an available ticket decoration
+	 *
+	 * @return boolean
+	 */
+	public function isAvailable($className) {
+		return array_key_exists($className, $this->_availableTicketDecorations);
+	}
 
-    /**
-     * Is $className an active ticket decoration
-     *
-     * @return boolean
-     */
-    public function isActive($className) {
-        return array_key_exists($className, $this->_activeTicketDecorations);
-    }
+	/**
+	 * Is $className an active ticket decoration
+	 *
+	 * @return boolean
+	 */
+	public function isActive($className) {
+		return array_key_exists($className, $this->_activeTicketDecorations);
+	}
 
-    /**
-     * Extract the specified ticket decorations from the list of available configurations
-     * and place them in the list of currently active decoration configurations
-     *
-     * @param $classNames array
-     * @return $this common\models\ticketDecoration\TicketDecorationManager
-     */
-    public function registerDecorations($classNames = null) {
-        $this->_activeTicketDecorations = []; // Start with empty list (reset) and add the new classes
+	/**
+	 * Extract the specified ticket decorations from the list of available configurations
+	 * and place them in the list of currently active decoration configurations
+	 *
+	 * @param $classNames array
+	 * @return $this common\models\ticketDecoration\TicketDecorationManager
+	 */
+	public function registerDecorations($classNames = null) {
+		$this->_activeTicketDecorations = []; // Start with empty list (reset) and add the new classes
 
-        if (is_array($classNames)) {
-            foreach ($classNames as $className) {
-                if (array_key_exists($className, $this->_availableTicketDecorations)) {
-                    $this->_activeTicketDecorations[$className] = $this->_availableTicketDecorations[$className];
-                }
-            }
-        }
+		if (is_array($classNames)) {
+			foreach ($classNames as $className) {
+				if (array_key_exists($className, $this->_availableTicketDecorations)) {
+					$this->_activeTicketDecorations[$className] = $this->_availableTicketDecorations[$className];
+				}
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 }
