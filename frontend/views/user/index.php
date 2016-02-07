a786890503b7b74511328ef5dfd8102df4b3cfd3
Andrew Potter <apc@andypotter.org>
2016-02-07 18:30:00 +0100

    Gii generated FE-User Code

---
+++ frontend/views/user/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html; use
+yii\grid\GridView; /* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */ $this->title =
+Yii::t('app', 'Users'); $this->params['breadcrumbs'][] = $this->title; ?>
+<div
+	class="user-index">
+
+<h1><?= Html::encode($this->title) ?></h1>
+
+<p><?= Html::a(Yii::t('app', 'Create User'),
+['create'], ['class' => 'btn btn-success']) ?></p>
+
+ <?= GridView::widget([
+'dataProvider' => $dataProvider, 'columns' => [
+['class' => 'yii\grid\SerialColumn'],             'id',
+            'username:ntext',
+            'password_hash:ntext',
+            'password_reset_token:ntext',
+            'email:ntext',
+            // 'auth_key:ntext',
+            // 'status',
+            // 'created_at',
+            // 'updated_at',
+            // 'password:ntext',
+            // 'board_id',
+ ['class' => 'yii\grid\ActionColumn'], ], ]); ?> </div>
---
+++ frontend/views/user/view.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\helpers\Html; use yii\widgets\DetailView; /* @var $this
+yii\web\View */ /* @var $model
+frontend\models\User*/ $this->title = $model->
+id; $this->params['breadcrumbs'][] = ['label' =>
+Yii::t('app', 'Users'), 'url' => ['index']]; $this->params['breadcrumbs'][] = $this->title; ?>
+<div
+	class="user-view">
+
+<h1><?= Html::encode($this->title) ?></h1>
+
+<p><?= Html::a(Yii::t('app', 'Update'),
+['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?> <?= Html::a(Yii::t('app', 'Delete'),
+['delete', 'id' => $model->id], [ 'class' => 'btn btn-danger', 'data' =>
+[ 'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+'method' => 'post', ], ]) ?></p>
+
+<?= DetailView::widget([ 'model' => $model, 'attributes' => [             'id',
+            'username:ntext',
+            'password_hash:ntext',
+            'password_reset_token:ntext',
+            'email:ntext',
+            'auth_key:ntext',
+            'status',
+            'created_at',
+            'updated_at',
+            'password:ntext',
+            'board_id',
+ ], ]) ?></div>
