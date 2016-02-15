591d741d162e1c14adb541fe7295f3391698d975
Andrew Potter <apc@andypotter.org>
2016-02-15 18:35:07 +0100

    New Decoratio for Creating ToDos

---
+++ common/models/ticketDecoration/CreateToDo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace common\models\ticketDecoration;
+
+/**
+ * Created by PhpStorm.
+ * User: and
+ * Date: 8/29/15
+ * Time: 12:33 AM
+ */
+
+class MoveToKanban extends AbstractDecoration {
+
+	public $linkIcon = 'K';
+
+	/*##################*/
+	/*### VIEW STUFF ###*/
+	/*##################*/
+
+	/**
+	 * Show a view of the Behavior
+	 * The default is the Icon Click element
+	 * A Decoration can have multiple views
+	 *
+	 * @return string html for showing the ticketDecoration
+	 */
+	public function show($view = 'default') {
+		return '<a data-toggle="tooltip" title="Move to KanBan Board" href="/ticket/board/' . $this->owner->id . '">' . $this->linkIcon .'</a>';
+	}
+
+}
