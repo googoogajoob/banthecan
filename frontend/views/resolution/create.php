a984935c12e402e874d633535fd2a3071ececba5
Andrew Potter <apc@andypotter.org>
2016-02-19 01:12:40 +0100

    Resolution Controller and Views added to FE

---
+++ frontend/views/resolution/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Resolution */
+
+$this->title = Yii::t('app', 'Create Resolution');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Resolutions'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="resolution-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('partials/_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
---
+++ frontend/views/resolution/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\ResolutionSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Resolutions');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="resolution-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Resolution'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+<?php Pjax::begin(); ?>    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'created_at',
+            'updated_at',
+            'created_by',
+            'updated_by',
+            'title:ntext',
+            'description:ntext',
+            'ticket_id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+<?php Pjax::end(); ?></div>
