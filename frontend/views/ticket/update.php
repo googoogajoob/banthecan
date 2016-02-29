946c8c7dd44394638d4064355b6e506d9ffb2cf8
Andrew Potter <apc@andypotter.org>
2016-02-29 20:09:58 +0100

    More update Ticket form

--- frontend/views/ticket/update.php
+++ frontend/views/ticket/update.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 $this->title = \Yii::t('app', 'Update Ticket');
 $this->params['breadcrumbs'][] = ['label' => \Yii::t('app', 'Tickets'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
+//$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
 //$this->params['breadcrumbs'][] = \Yii::t('app', 'Update');
 ?>
 <div class="ticket-update">
