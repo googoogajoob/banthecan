4ed812b7c396a269c6fae529f488a43043f16b27
Andrew Potter <apc@andypotter.org>
2016-03-18 23:31:44 +0100

    Blame Display when user not found

--- frontend/views/user/partials/_blame.php
+++ frontend/views/user/partials/_blame.php
@@ -10,9 +10,21 @@ use yii\helpers\Html;
 ?>
 
 <div class="pull-left img-responsive" style="margin-right: 4px;">
-    <?php echo Html::img($avatar); ?>
+    <?php
+        if ($avatar) {
+            echo Html::img($avatar);
+        }
+    ?>
 </div>
-<strong><em><?php echo $name; ?></em></strong>
+<strong>
+    <em>
+        <?php
+            if ($name) {
+                echo $name;
+            }
+        ?>
+    </em>
+</strong>
 
 <?php if (isset($timestamp)) : ?>
     <br/>
