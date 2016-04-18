3d3000fccffd43105ed3efc8c337fb09378eaaf6
Andrew Potter <apc@andypotter.org>
2016-04-18 23:00:45 +0200

    Part two, reformatting

--- frontend/views/layouts/right.php
+++ frontend/views/layouts/right.php
@@ -14,40 +14,40 @@ use common\models\Board;
 
 AppAsset::register($this);
 if ($boardObject = Board::getActiveBoard()) {
-	$this->title = $boardObject->title;
+    $this->title = $boardObject->title;
 } else {
-	$this->title = '';
+    $this->title = '';
 }
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-<meta charset="<?= Yii::$app->charset ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?= Html::csrfMetaTags() ?>
-<title>Ban the Can(<?= (YII_ENV_DEMO ? 'DEMO' : '') ?>): <?= Html::encode($this->title) ?></title>
-<?php $this->head() ?>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title>Ban the Can(<?= (YII_ENV_DEMO ? 'DEMO' : '') ?>): <?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
 <div class="wrap"><?php
-echo $this->renderFile('@frontend/views/layouts/partials/_navigation.php', ['boardObject' => $boardObject]);
-echo $this->renderFile('@frontend/views/layouts/partials/_right-sidebar.php');
-?>
+    echo $this->renderFile('@frontend/views/layouts/partials/_navigation.php', ['boardObject' => $boardObject]);
+    echo $this->renderFile('@frontend/views/layouts/partials/_right-sidebar.php');
+    ?>
 
-<div id="layout-main" class="right-layout-main"><?php
-echo Html::icon('circle-arrow-left', [
-                    'id' => 'toggle-right-sidebar',
-                    'class' => 'pull-right apc-layout-toggle-button',
-]);
-?>
+    <div id="layout-main" class="right-layout-main"><?php
+        echo Html::icon('circle-arrow-left', [
+            'id' => 'toggle-right-sidebar',
+            'class' => 'pull-right apc-layout-toggle-button',
+        ]);
+        ?>
 
-<div class="container-fluid"><?php echo Breadcrumbs::widget([
-                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-]) ?> <?php echo Alert::widget(); ?> <?php echo $content ?></div>
+        <div class="container-fluid"><?php echo Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?><?php echo Alert::widget(); ?><?php echo $content ?></div>
 
-</div>
+    </div>
 
 </div>
 
