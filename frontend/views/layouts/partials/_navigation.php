c03f5991a58979a0ee505dfee84dcfa65fd840df
Andrew Potter <apc@andypotter.org>
2016-08-14 13:46:05 +0200

    Foundation step one

--- frontend/views/layouts/partials/_navigation.php
+++ frontend/views/layouts/partials/_navigation.php
@@ -39,7 +39,7 @@ if (Yii::$app->user->isGuest) {
 			['label' => \Yii::t('app', 'User Settings'), 'url' => ['/user/view']],
 			['label' => \Yii::t('app', 'Contact'), 'url' => ['/site/contact']],
 			['label' => \Yii::t('app', 'About'), 'url' => ['/site/about']],
-		],
+		]
 	];
 
 	$menuItems[] = html::tag('li',
@@ -47,7 +47,56 @@ if (Yii::$app->user->isGuest) {
 		['class' => 'menu-avatar-li']);
 
 }
+?>
 
+<!-- div class="top-bar">
+<div class="top-bar-right" -->
+<!-- ul class="menu dropdown" data-dropdown-menu -->
+
+    <ul class="dropdown menu" data-dropdown-menu>
+        <li>
+            <a href="#">Item 1</a>
+            <ul class="menu">
+                <li><a href="#">Item 1A</a></li>
+                <!-- ... -->
+            </ul>
+        </li>
+        <li><a href="#">Item 2</a></li>
+        <li><a href="#">Item 3</a></li>
+        <li><a href="#">Item 4</a></li>
+    </ul>
+<!-- ?php
+    foreach ($menuItems as $v) {
+
+        if (!is_array($v)) {
+
+            echo $v;
+
+        } elseif (array_key_exists('url', $v)) {
+
+            echo '<li><a href ="' . array_shift($v['url']) . '">' . $v['label'] . '</a ></li>';
+
+        } elseif (array_key_exists('items', $v)) {
+
+            echo '<li>';
+            echo '<a href ="#">' . $v['label'] . '</a >';
+            echo '<ul class="menu">';
+
+            foreach ($v['items'] as $sv) {
+                echo '<li><a href ="' . array_shift($sv['url']) . '">' . $sv['label'] . '</a ></li>';
+            }
+
+            echo '</ul>';
+            echo '</li>';
+        }
+    }
+? -->
+
+<!-- /ul -->
+<!-- /div>
+</div -->
+
+<?php
 /*echo Nav::widget([
 	'options' => ['class' => 'navbar-nav navbar-right'],
 	'items' => $menuItems,
