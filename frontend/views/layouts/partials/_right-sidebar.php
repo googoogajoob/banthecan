808702749698269936c7897025dca425cf7d71a0
Andrew Potter <apc@andypotter.org>
2016-09-18 01:03:45 +0200

    Right Column Button only visible in Large (div area yet to come)

--- frontend/views/layouts/partials/_right-sidebar.php
+++ frontend/views/layouts/partials/_right-sidebar.php
@@ -8,11 +8,13 @@
 ?>
 
 <div id="right-layout-sidebar">
-<div class="container-fluid"><?php
-if ($this->blocks && array_key_exists('right-sidebar', $this->blocks)) {
-	echo $this->blocks['right-sidebar'];
-}
-?></div>
+	<div class="container-fluid">
+		<?php
+		if ($this->blocks && array_key_exists('right-sidebar', $this->blocks)) {
+			echo $this->blocks['right-sidebar'];
+		}
+		?>
+	</div>
 </div>
 
 
