0e1ab19c845a89e851115baf107700b232c73428
Andrew Potter <apc@andypotter.org>
2016-09-22 18:49:41 +0200

    oops forgot to include the class

--- frontend/views/layouts/left-right.php
+++ frontend/views/layouts/left-right.php
@@ -9,6 +9,7 @@ use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
 use common\widgets\Alert;
 use common\models\Board;
+use yii\bootstrap\Modal;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
