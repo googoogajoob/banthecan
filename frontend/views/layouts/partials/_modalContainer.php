27cb723fbb377479b0952ce455ca3348e38270ce
Andrew Potter <apc@andypotter.org>
2016-09-25 12:03:11 +0200

    Modal Header and Button

--- frontend/views/layouts/partials/_modalContainer.php
+++ frontend/views/layouts/partials/_modalContainer.php
@@ -5,6 +5,10 @@ use yii\bootstrap\Modal;
 Modal::begin([
     'id' => 'global-modal-container',
     'size' => 'modal-lg',
+    'closeButton' => [
+        'class' => 'btn btn-danger pull-right glyphicon glyphicon-remove',
+        'label' => '',
+    ]
     //'header' => '<h2>Global Modal Container - Header</h2>',
     //'footer' => '<em>Global Modal Container - Footer</em>'
 ]);
