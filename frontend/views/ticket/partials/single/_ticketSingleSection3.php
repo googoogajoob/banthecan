ef8a85ad09efd948f68077c567b52b56b18e14b8
Andrew Potter <apc@andypotter.org>
2016-11-06 18:40:48 +0100

    Code reformatting

--- frontend/views/ticket/partials/single/_ticketSingleSection3.php
+++ frontend/views/ticket/partials/single/_ticketSingleSection3.php
@@ -14,15 +14,16 @@ use yii\helpers\Html;
     }
 
     foreach ($model->getBehaviors() as $ticketBehavior) {
+
         if ($ticketBehavior instanceof TicketDecorationInterface) {
-            echo Html::beginTag('div', ['class' => 'ticket-single-decorations-glyph']);
 
-            echo $ticketBehavior->show();
+            if (!$ticketBehavior->displaySection) {
 
-            $junk = $ticketBehavior->displaySection;
-            echo $junk ? $junk : '?';
-            
-            echo Html::endTag('div');
+                echo Html::tag('div', $ticketBehavior->show(), [
+                    'class' => 'ticket-single-decorations-glyph']
+                );
+
+            }
         }
     }
 
