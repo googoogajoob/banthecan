eed9be3a7a8e9a9acf6ac3ac0559b7c245089a3f
Andrew Potter <apc@andypotter.org>
2016-12-20 17:30:22 +0100

    Bootstrap Tooltips working in Backlog, Completed

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -26,6 +26,7 @@ class AppAsset extends AssetBundle
 	];
 	public $depends = [
         'yii\web\YiiAsset',
+		'yii\jui\JuiAsset',
         'yii\bootstrap\BootstrapAsset',
         'yii\bootstrap\BootstrapPluginAsset',
 	];
