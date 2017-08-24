8bd5270173e2be6c91bad67f0bd381abe8519e98
Andrew Potter <apc@andypotter.org>
2017-08-24 18:55:03 +0200

    Formatting

--- console/config/main.php
+++ console/config/main.php
@@ -1,9 +1,9 @@
 <?php
 $params = array_merge(
-require(__DIR__ . '/../../common/config/params.php'),
-require(__DIR__ . '/../../common/config/params-local.php'),
-require(__DIR__ . '/params.php'),
-require(__DIR__ . '/params-local.php')
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
 );
 
 return [
@@ -13,16 +13,16 @@ return [
     'controllerNamespace' => 'console\controllers',
     'modules' => [
         'gii' => 'yii\gii\Module',
-],
+    ],
     'components' => [
         'log' => [
             'targets' => [
-[
+                [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
-],
-],
-],
-],
+                ],
+            ],
+        ],
+    ],
     'params' => $params,
 ];
