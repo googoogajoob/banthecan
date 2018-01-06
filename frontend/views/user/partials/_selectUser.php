557f4ad789d8cbc1712cad2f5512ba6a2dbfcd69
and <and@lagavulin.ahp.home>
2018-01-06 19:30:44 +0100

    Users available for tasks, Menu button colors

--- frontend/views/user/partials/_selectUser.php
+++ frontend/views/user/partials/_selectUser.php
@@ -17,40 +17,42 @@ TaskAsset::register($this);
 ?>
 
 <?php
-$users = User::getBoardUsers();
-$radioSetup = [];
-foreach ($users as $user) {
-    // Include color and grayscale avatar, color when selected, gray when not selected
-    // visibility toggled via jQuery/javascript
-    $colorId = 'user_id-avatar-color-' . $user->id;
-    $grayId = 'user_id-avatar-gray-' . $user->id;
-    $radioSetup[$user->id] =
-        html::img($user->avatarUrlColor, [
-            'alt' => $user->username,
-            'title' => $user->username,
-            'id' => $colorId,
-            'class' => $user->id != $model->user_id ? 'user-avatar-hide' : '',
-        ])
-        . html::img($user->avatarUrlGray, [
-            'alt' => $user->username,
-            'title' => $user->username,
-            'id' => $grayId,
-            'class' => $user->id == $model->user_id ? 'user-avatar-hide' : '',
-        ]);
-}
+    $users = User::getBoardUsers();
+    $radioSetup = [];
 
-echo $form->field($model, 'user_id', ['options' => ['class' => 'clearfix']])
-    ->radioList($radioSetup, [
-            'item' =>
-                function ($index, $label, $name, $checked, $value) {
-                    $inlineJS = 'taskUser(' . $value . ')';
-                    return '<div class="radio">'
-                    . Html::radio($name, $checked, [
-                        'label' => $label,
-                        'value' => $value,
-                        'onchange' => $inlineJS,
-                    ]) . '</div>';
-                }
-        ]
-    );
+    foreach ($users as $user) {
+        // Include color and greyscale avatar,
+        // color when selected, gray when not selected
+        // visibility toggled via jQuery/javascript
+        $colorId = 'user_id-avatar-color-' . $user->id;
+        $grayId = 'user_id-avatar-gray-' . $user->id;
+        $radioSetup[$user->id] =
+            html::img($user->avatarUrlColor, [
+                'alt' => $user->username,
+                'title' => $user->username,
+                'id' => $colorId,
+                'class' => $user->id != $model->user_id ? 'user-avatar-hide' : '',
+            ])
+            . html::img($user->avatarUrlGray, [
+                'alt' => $user->username,
+                'title' => $user->username,
+                'id' => $grayId,
+                'class' => $user->id == $model->user_id ? 'user-avatar-hide' : '',
+            ]);
+    }
+
+    echo $form->field($model, 'user_id', ['options' => ['class' => 'clearfix']])
+        ->radioList($radioSetup, [
+                'item' =>
+                    function ($index, $label, $name, $checked, $value) {
+                        $inlineJS = 'taskUser(' . $value . ')';
+                        return '<div class="radio">'
+                        . Html::radio($name, $checked, [
+                            'label' => $label,
+                            'value' => $value,
+                            'onchange' => $inlineJS,
+                        ]) . '</div>';
+                    }
+            ]
+        );
 ?>
