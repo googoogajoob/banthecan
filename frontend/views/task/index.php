f1a5af756f02a7a6813e1c54fc0b9fa68f7f3f24
and <and@lagavulin.ahp.home>
2018-01-07 19:15:35 +0100

    Filtering for tasks implemented

--- frontend/views/task/index.php
+++ frontend/views/task/index.php
@@ -1,8 +1,12 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use common\models\User;
+use common\models\Ticket;
+
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\TaskSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -18,43 +22,69 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php Pjax::begin(); ?>
 
-        <?php echo GridView::widget([
-            'dataProvider' => $dataProvider,
-            'filterModel' => $searchModel,
-            'columns' => [
-                [
-                    'format' => 'raw',
-                    'label' => \Yii::t('app', 'Responsible'),
-                    'content' => function ($model, $key, $index, $column) {
-                        return $this->render('@frontend/views/user/partials/_blame', [
-                                'model' => $model,
-                            ]
-                        );
-                    },
-                ],
-                [
-                    'attribute' => 'completed',
-                    'format' => 'boolean',
-                    'label' => \Yii::t('app', 'Completed'),
-                ],
-                [
-                    'attribute' => 'title',
-                    'format' => 'ntext',
-                    'label' => \Yii::t('app', 'Title'),
-                ],
-                [
-                    'attribute' => 'description',
-                    'format' => 'ntext',
-                    'label' => \Yii::t('app', 'Description'),
-                ],
-                [
-                    'attribute' => 'ticket.title',
-                    'format' => 'ntext',
-                    'label' => \Yii::t('app', 'Ticket'),
-                ],
-                ['class' => 'yii\grid\ActionColumn'],
+    <?php
+
+    $allBoardUsers = User::getBoardUsers();
+    $allBoardUsernames = ArrayHelper::map($allBoardUsers, 'id', 'username');
+
+    $allTaskTickets = Ticket::findTicketsInTaskColumns();
+    $allTaskTicketTitles = ArrayHelper::map($allTaskTickets, 'id', 'title');
+
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            [
+                'format' => 'raw',
+                'label' => \Yii::t('app', 'Responsible'),
+                'content' => function ($model, $key, $index, $column) {
+                    return $this->render('@frontend/views/user/partials/_blame', [
+                            'model' => $model,
+                        ]
+                    );
+                },
+                'filter' => $allBoardUsernames,
+                'attribute' => 'user_id',
             ],
-        ]);
+            [
+                'attribute' => 'completed',
+                'format' => 'boolean',
+                'label' => \Yii::t('app', 'Completed'),
+                'filter' => [
+                        Yii::$app->getFormatter()->asBoolean(0),
+                        Yii::$app->getFormatter()->asBoolean(1),
+                    ],
+            ],
+            [
+                'attribute' => 'title',
+                'format' => 'ntext',
+                'label' => \Yii::t('app', 'Title'),
+            ],
+            [
+                'attribute' => 'description',
+                'format' => 'ntext',
+                'label' => \Yii::t('app', 'Description'),
+            ],
+            [
+                'format' => 'ntext',
+                'label' => \Yii::t('app', 'Ticket'),
+                'content' => function ($model, $key, $index, $column) {
+                    $ticket = $model->getTicket()->one();
+                    return Html::a(
+                            $ticket->title,
+                            '/ticket/view/' . $ticket->id,
+                            [
+                                'data-toggle' => 'modal',
+                                'data-target' => '#global-modal-container',
+                            ]
+                    );
+                },
+                'filter' => $allTaskTicketTitles,
+                'attribute' => 'ticket_id',
+            ],
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]);
     ?>
 
     <?php Pjax::end(); ?>
