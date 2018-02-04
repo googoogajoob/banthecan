a36fd3c5209b8bef9a8b4d2a30e00b0ed9cd3fbf
and <and@lagavulin.ahp.home>
2018-02-04 20:11:56 +0100

    Filter eingebaut

--- backend/views/column/index.php
+++ backend/views/column/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 use yii\grid\GridView;
 use backend\assets\ColumnAsset;
-use common\models\Column;
+use common\models\Board;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\ColumnSearch */
@@ -17,40 +17,54 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="board-column-index">
 
-    <h1><?php echo Html::encode($this->title) ?></h1>
+    <h1>
+        <?php echo Html::encode($this->title) ?>
+    </h1>
 
-    <p><?php echo Html::a(\Yii::t('app', 'Create Board Column'), ['create'], ['class' => 'btn btn-success']) ?>
+    <p>
+        <?php echo Html::a(\Yii::t('app', 'Create Board Column'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php echo GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'tableOptions' => ['id' => 'sort', 'class' => 'table table-striped table-bordered'],
-        'rowOptions' => function ($model, $key, $index, $grid) {
-            return ['id' => 'row_' . $key, 'display-order' => $model->display_order];
-        },
-        'columns' => [
-            ['class' => 'yii\grid\ActionColumn'],
-            'created_at:datetime',
-            'updated_at:datetime',
-            'board_id',
-            'boardTitle',
-            'title:ntext',
-            'receiverList:ntext:Receiver',
-            [
-                'label' => \Yii::t('app', 'Decorations'),
-                'format' => 'raw',
-                'value' => function ($model, $key, $index, $column) {
-                    if (is_array($model->ticket_column_configuration)) {
-                        return implode(', ', $model->ticket_column_configuration);
-                    } else {
-                        return '';
-                    }
+    <?php
 
-                },
+        $allBoards = Board::find()->orderBy('title')->all();
+        $allBoardTitles = ArrayHelper::map($allBoards, 'id', 'title');
+
+        echo GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'tableOptions' => ['id' => 'sort', 'class' => 'table table-striped table-bordered'],
+            'rowOptions' => function ($model, $key, $index, $grid) {
+                return ['id' => 'row_' . $key, 'display-order' => $model->display_order];
+            },
+            'columns' => [
+                ['class' => 'yii\grid\ActionColumn'],
+                'created_at:datetime',
+                'updated_at:datetime',
+                [
+                    'format' => 'ntext',
+                    'label' => 'Board',
+                    'value' => function ($model, $key, $index, $column) {
+                        return $model->boardTitle;
+                    },
+                    'attribute' => 'board_id',
+                    'filter' => $allBoardTitles,
+                    'filterInputOptions' => ['class' => 'form-control form-control-task-column'],
+                ],
+                'title:ntext',
+                'receiverList:ntext:Receiver',
+                [
+                    'label' => \Yii::t('app', 'Decorations'),
+                    'format' => 'raw',
+                    'value' => function ($model, $key, $index, $column) {
+                        if (is_array($model->ticket_column_configuration)) {
+                            return implode(', ', $model->ticket_column_configuration);
+                        } else {
+                            return '';
+                        }
+                    },
+                ],
             ],
-        ],
-    ]);
-
+        ]);
     ?>
 </div>
