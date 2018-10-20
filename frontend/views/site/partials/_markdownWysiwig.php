71d50d2be3281e4b8580af900450b8d8cf834573
and <apc@andypotter.org>
2018-10-20 10:40:47 +0200

    Wysiwig Emoji Insert Buttons added

--- frontend/views/site/partials/_markdownWysiwig.php
+++ frontend/views/site/partials/_markdownWysiwig.php
@@ -1,9 +1,31 @@
 <?php
+
+use apc\markdown\Markdown;
 /* @var $inputId string */
+
 ?>
 <div class="markdown-wysiwig-container">
     <div class="col-sm-offset-2 col-sm-6 bg-info markdown-wysiwig">
-        <a onclick="wysiwigEdit('<?php echo $inputId ?>', 'junk text');"><span class="glyphicon glyphicon-plus"></span></a>
+        <?php
+            $iconList = [
+                ':apc-checkmark:',
+                ':apc-x:',
+                ':apc-smiley:',
+                ':apc-worried:',
+                ':apc-thumbsup:',
+                ':apc-thumbsdown:',
+            ];
+
+            $outputList = [];
+            foreach ($iconList as $emojiCode) {
+                $outputHtml = Markdown::process($emojiCode);
+                $anchorTag = '<a onclick=' . "\"wysiwigEdit('$inputId', '$emojiCode');\"" . '>';
+                $outputHtml = str_replace('<p>', $anchorTag, $outputHtml);
+                $outputHtml = str_replace('</p>', '</a>', $outputHtml);
+                $outputList[] = $outputHtml;
+            }
+            echo implode('', $outputList);
+        ?>
     </div>
 </div>
 
